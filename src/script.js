82d1ca1e020979b1a5ec94ff643ce3a0924b7346
mnunezsa <mnunezsa95@gmail.com>
2023-09-01 18:02:01 -0400

    create workout class

--- src/script.js
+++ src/script.js
@@ -1,9 +1,46 @@
 import { months, form, containerWorkouts, inputType, inputDistance, inputDuration, inputCadence, inputElevation } from "./utils/constant.js";
 
-/* ---------------------------------------------------------------------------------------------- */
-/*                                        Global variables                                        */
-/* ---------------------------------------------------------------------------------------------- */
+class Workout {
+  constructor(coords, distance, duration) {
+    this._coords = coords; // array of coordinates
+    this._distance = distance; // in Km
+    this._duration = duration; // in min
+    this.date = new Date();
+    this._id = Math.trunc(Math.random() * 100);
+  }
+}
 
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this._duration / this._distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, evelationGain) {
+    super(coords, distance, duration);
+    this.evelationGain = evelationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this._distance / (this._duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycle1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycle1);
+
+// App Architecture
 class App {
   constructor() {
     this._map;
